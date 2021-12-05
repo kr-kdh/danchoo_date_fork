31e025b220eafaa9f0fa6130c1518b3c6b73235b
Danchoo21 <60216437+SeongMyeongYun@users.noreply.github.com>
2021-12-05 18:55:15 +0900

    floating button margin

--- app/src/main/java/com/danchoo/date/presentation/ui/components/MainApp.kt
+++ app/src/main/java/com/danchoo/date/presentation/ui/components/MainApp.kt
@@ -1,6 +1,7 @@
 package com.danchoo.date.presentation.ui.components
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -18,10 +19,10 @@ fun MainApp() {
 
     Scaffold(
         bottomBar = { MainBottomBar(navController = navController, tabs = tabs) },
-    ) {
+    ) { innerPaddingModifier ->
         MainNavGraph(
             navController = navController,
-            modifier = Modifier
+            modifier = Modifier.padding(innerPaddingModifier)
         )
     }
 }
