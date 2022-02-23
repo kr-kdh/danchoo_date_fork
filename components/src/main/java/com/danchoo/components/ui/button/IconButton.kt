a809a31c543ca766f8ac99cf9e1b51296350de3c
danchoo21 <danchoo45@naver.com>
2022-02-23 21:19:40 +0900

    fix: expendButton disable tint color

--- components/src/main/java/com/danchoo/components/ui/button/IconButton.kt
+++ components/src/main/java/com/danchoo/components/ui/button/IconButton.kt
@@ -1,10 +1,7 @@
 package com.danchoo.components.ui.button
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.LocalContentColor
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -18,9 +15,14 @@ fun IconButton(
     imageVector: ImageVector,
     enabled: Boolean = true,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
+    tint: Color? = null,
     onClick: () -> Unit
 ) {
+    val contentTint = tint ?: run {
+        val contentAlpha = if (enabled) LocalContentAlpha.current else ContentAlpha.disabled
+        LocalContentColor.current.copy(alpha = contentAlpha)
+    }
+
     IconButton(
         modifier = modifier,
         enabled = enabled,
@@ -29,7 +31,7 @@ fun IconButton(
     ) {
         Icon(
             imageVector = imageVector,
-            tint = tint,
+            tint = contentTint,
             contentDescription = null
         )
     }
