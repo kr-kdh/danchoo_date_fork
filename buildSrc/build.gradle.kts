e49381efcd026ea7aa178b0e320afead51e7cfaf
danchoo21 <danchoo45@naver.com>
2022-02-25 23:50:42 +0900

    chore: Gradle version update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.1.1")
+    implementation("com.android.tools.build:gradle:7.1.2")
     implementation(kotlin("gradle-plugin", "1.6.10"))
 }
